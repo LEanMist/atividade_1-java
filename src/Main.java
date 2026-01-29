f672d42ef5b31c23616f5ed01b13ced3968b4b45
LEanMist <leandro.chq123@gmail.com>
2026-01-29 11:10:33 -0300

    lista de nomes com input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String nome = "LELE";
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add(nome);
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Digite seu nome: ");
+            String novoNome = sc.nextLine();
+            lista.add(novoNome);
+            imprimir(lista);
+        }
+    }
+
+    public static void imprimir(ArrayList<String> lista){
+        for(String nomeLista :lista){
+            System.out.println(nomeLista);
+        }
     }
 }
+
